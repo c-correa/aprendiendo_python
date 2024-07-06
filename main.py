80433e63bafd06b3e909265dee31ca74dccca54d
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2024-07-05 23:20:20 -0500

    Update main.py
    add more examples

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 #implementation the diferents list comprehenstions 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+text = ["hola", "como", "estas?"]
+number_in_text = ["1", "2", "3", "4", "9", "6", "7"]
+dic = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 9}
 
 #1.
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers_v2 = [i ** 2 for i in range(len(numbers)) if i % 2 != 0]
 print(numbers_v2)
 #2.
-text = ["hola", "como", "estas?"]
 text_v2 = [i.upper() for i in text]
 print(text_v2)
 #3.
-number_in_text = ["1", "2", "3", "4", "5", "6", "7", "8", "9",]
 converting_text_to_number = [+i for i in range(len(number_in_text))]
 print(converting_text_to_number)
 
 # Dictonary comprehenstions
 #1.
-dic = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
 new_dic = { value: key for key, value in dic.items() }
 print(new_dic)
 #2.
 converting_list_to_dic = { i: i **2  for i in range(len(numbers)) }
 print(converting_list_to_dic)
 #3.
-dic_v2 = {"a":1, "b":2, "c":3, "d":4, "e":5}
-filter = {key: value for key, value in dic_v2.items() if value > 4}
+filter = {key: value for key, value in dic.items() if value > 4}
 print(filter)
 
 #combined
 #1.
+list_of_tuples = {(key, value) for key, value in dic.items()}
+print(list_of_tuples)
+#2.
+list_to_dic = {text[i]: len(text[i]) for i in range(len(text))}
+print(list_to_dic)
+#3.
+converting_string = [str(value) for key, value in dic.items()]
+print(converting_string)
+# Or
+converting_string = [str(value) for value in dic.values()]
+print(converting_string)
